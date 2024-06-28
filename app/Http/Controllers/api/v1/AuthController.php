d7a7a8b0d70f72c931d4258b09545b6791f7126c
Kava Developers <mehul9921@gmail.com>
2024-06-28 11:21:08 +0530

    Update AuthController.php

--- app/Http/Controllers/api/v1/AuthController.php
+++ app/Http/Controllers/api/v1/AuthController.php
@@ -17,26 +17,28 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Symfony\Component\HttpFoundation\Response;
 
-class AuthController extends Controller{
-    
-    public function login(Request $request) : Response {
-        if(!$request->username){
-            return CommonHelper::response('0',['message' => '`username` is reqiured.']);
-        }else if(!$request->password){
-            return CommonHelper::response('0',['message' => '`password` is reqiured.']);
-        }else{
-            $user = UserApiModel::where('username',$request->username)->where('is_deleted','0')->where('role','1')->first();
-            if(!$user){
-                return CommonHelper::response('0',['message' => '`username` not registered']);
-            }else{
-                if($user->is_blocked == '1'){
-                    return CommonHelper::response('0',['message' => 'Your account is blocked please contact administrator.']);
-                }else{
-                    if(!Hash::check($request->password,$user->password)){
-                        return CommonHelper::response('0',['message' => 'Username and Password not match.']);
-                    }else{
+class AuthController extends Controller
+{
+
+    public function login(Request $request): Response
+    {
+        if (!$request->username) {
+            return CommonHelper::response('0', ['message' => '`username` is reqiured.']);
+        } else if (!$request->password) {
+            return CommonHelper::response('0', ['message' => '`password` is reqiured.']);
+        } else {
+            $user = UserApiModel::where('username', $request->username)->where('is_deleted', '0')->first();
+            if (!$user) {
+                return CommonHelper::response('0', ['message' => '`username` not registered']);
+            } else {
+                if ($user->is_blocked == '1') {
+                    return CommonHelper::response('0', ['message' => 'Your account is blocked please contact administrator.']);
+                } else {
+                    if (!Hash::check($request->password, $user->password)) {
+                        return CommonHelper::response('0', ['message' => 'Username and Password not match.']);
+                    } else {
                         $user->token = $user->createToken("Personal Access Token")->plainTextToken;
-                        return CommonHelper::response('1',[
+                        return CommonHelper::response('1', [
                             'message' => 'Login Success',
                             'data'    => $user
                         ]);
